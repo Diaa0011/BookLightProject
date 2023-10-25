c8e59986ad321fafd7d3ab8598430ecaa9a78c6b
Di63 <diaa10010079@gmail.com>
2023-10-25 15:15:46 +0300

    renaming Porject

---
+++ BookLight.Models/ViewModels/UserRoleVM.cs
@@ -0,0 +1,18 @@
+﻿using BookLight.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLight.Models.ViewModels
+{
+    public class UserRoleVM
+    {
+        public ApplicationUser applicationUser { get; set; }
+        public IEnumerable<SelectListItem> roleList { get; set; }
+        public IEnumerable<SelectListItem> companyList { get; set; }
+        public string currentRole { get; set; }
+    }
+}
